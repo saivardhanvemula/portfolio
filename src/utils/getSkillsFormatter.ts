d4ec6a8703e630afdcd955173aebcb68552f1742
saivardhanvemula <saivardhanvemulamncl@gmail.com>
2025-07-25 22:41:42 +0530

    fix: added alignment for getskillsFormatter

--- src/utils/getSkillsFormatter.ts
+++ src/utils/getSkillsFormatter.ts
@@ -1,6 +1,7 @@
 export function getSkillsFormatter(data: { domain: string; skills: string[] }[]) {
+    const maxLen = Math.max(...data.map(item => item.domain.length));
     const rows = data.map(
-        (item) => `${item.domain} - ${item.skills.join(", ")}`
+        (item) => `${item.domain.padEnd(maxLen)} - ${item.skills.join(", ")}`
     );
     return rows;
 }
